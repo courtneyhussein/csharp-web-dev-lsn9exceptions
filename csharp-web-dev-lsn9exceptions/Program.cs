6ee8b324404ef81ec13e988c867cf1f817f9d7e0
CourtneyHussein <courtneyhussein124@gmail.com>
2021-06-22 22:55:30 -0400

    Solved exceptions exercises

--- csharp-web-dev-lsn9exceptions/Program.cs
+++ csharp-web-dev-lsn9exceptions/Program.cs
@@ -7,28 +7,80 @@ namespace csharp_web_dev_lsn9exceptions
 {
     class Program
     {
-        /*
+        //Method for division.
         static double Divide(double x, double y)
         {
-            // Write your code here!
+            return x / y;
         }
-
+        
+        //Method to check file extension
         static int CheckFileExtension(string fileName)
         {
-            // Write your code here!
+            try
+            {
+                if (fileName == null || fileName == "")
+                {
+                    throw new ArgumentException("Parameter must not be null or an empty string.");
+                }
+                else if (fileName.Substring(fileName.Length - 3, 3) == ".cs")
+                {
+                    return 1;
+
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine("hi");
+                return 0;
+            }
+            
         }
-        */
+        
 
         static void Main(string[] args)
         {
-            // Test out your Divide() function here!
+            //Prompt user to enter two methods.
+            Console.WriteLine("Enter a number: ");
+            double x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter a number: ");
+            double y = Convert.ToDouble(Console.ReadLine());
 
-            // Test out your CheckFileExtension() function here!
+            try
+            {
+                if (y == 0)
+                {
+                    throw new ArgumentOutOfRangeException("Value cannot equal zero.");
+                }
+                else
+                {
+                    Console.WriteLine(Divide(x, y));
+                }
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+
+            Console.WriteLine();//Print empty line for formatting.
+
+            
             Dictionary<string, string> students = new Dictionary<string, string>();
             students.Add("Carl", "Program.cs");
             students.Add("Brad", "");
             students.Add("Elizabeth", "MyCode.cs");
             students.Add("Stefanie", "CoolProgram.cs");
+
+            //Call the method on each of the file names in the dictionary.
+            foreach (var pair in students)
+            {
+                Console.WriteLine($"Score for proper file extention: {CheckFileExtension(pair.Value)}");
+            }
 
 
         }
